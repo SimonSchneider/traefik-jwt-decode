6543aad3cc6c21c1bd4e653203449bfeec2215c2
Simon Schneider <dev@simonschneider.org>
2020-06-14 19:57:43 +0200

    fix configuration

--- config/config.go
+++ config/config.go
@@ -17,50 +17,60 @@ import (
 	"github.com/rs/zerolog"
 )
 
+// Env variable constants
 const (
-	JwksUrlEnv                                   = "JWKS_URL"
-	ClaimMappingFileEnv, ClaimMappingFileDefault = "CLAIM_MAPPING_FILE_PATH", "config.json"
-	AuthHeaderEnv, AuthHeaderDefault             = "AUTH_HEADER_KEY", "Authorization"
-	PortEnv, PortDefault                         = "PORT", "8080"
-	LogLevelEnv, LogLevelDefault                 = "LOG_LEVEL", "info"
-	LogTypeEnv, LogTypeDefault                   = "LOG_TYPE", "json"
-	MaxCacheKeysEnv, MaxCacheKeysDefault         = "MAX_CACHE_KEYS", "10000"
-	ClaimMappingsEnv                             = "CLAIM_MAPPINGS"
+	JwksURLEnv              = "JWKS_URL"
+	ClaimMappingFileEnv     = "CLAIM_MAPPING_FILE_PATH"
+	ClaimMappingFileDefault = "config.json"
+	AuthHeaderEnv           = "AUTH_HEADER_KEY"
+	AuthHeaderDefault       = "Authorization"
+	PortEnv                 = "PORT"
+	PortDefault             = "8080"
+	LogLevelEnv             = "LOG_LEVEL"
+	LogLevelDefault         = "info"
+	LogTypeEnv              = "LOG_TYPE"
+	LogTypeDefault          = "json"
+	MaxCacheKeysEnv         = "MAX_CACHE_KEYS"
+	MaxCacheKeysDefault     = "10000"
+	ClaimMappingsEnv        = "CLAIM_MAPPINGS"
 )
 
+// Config to bootstrap decoder server
 type Config struct {
-	JwksURL              envVar
-	ClaimMappingFilePath envVar
-	AuthHeader           envVar
-	Port                 envVar
-	LogLevel             envVar
-	LogType              envVar
-	ClaimMappings        envVar
-	MaxCacheKeys         envVar
+	jwksURL              envVar
+	claimMappingFilePath envVar
+	authHeader           envVar
+	port                 envVar
+	logLevel             envVar
+	logType              envVar
+	claimMappings        envVar
+	maxCacheKeys         envVar
 	keyCost              int64
 }
 
+// NewConfig creates a new Config from the current env
 func NewConfig() *Config {
 	var c Config
-	c.JwksURL = envVar{JwksUrlEnv, "", true}
-	c.ClaimMappingFilePath = envVar{ClaimMappingFileEnv, ClaimMappingFileDefault, true}
-	c.AuthHeader = envVar{AuthHeaderEnv, AuthHeaderDefault, true}
-	c.Port = envVar{PortEnv, PortDefault, true}
-	c.LogLevel = envVar{LogLevelEnv, LogLevelDefault, true}
-	c.LogType = envVar{LogTypeEnv, LogTypeDefault, true}
-	c.ClaimMappings = envVar{ClaimMappingsEnv, "", false}
-	c.MaxCacheKeys = envVar{MaxCacheKeysEnv, MaxCacheKeysDefault, true}
+	c.jwksURL = envVar{JwksURLEnv, "", true}
+	c.claimMappingFilePath = envVar{ClaimMappingFileEnv, ClaimMappingFileDefault, true}
+	c.authHeader = envVar{AuthHeaderEnv, AuthHeaderDefault, true}
+	c.port = envVar{PortEnv, PortDefault, true}
+	c.logLevel = envVar{LogLevelEnv, LogLevelDefault, true}
+	c.logType = envVar{LogTypeEnv, LogTypeDefault, true}
+	c.claimMappings = envVar{ClaimMappingsEnv, "", false}
+	c.maxCacheKeys = envVar{MaxCacheKeysEnv, MaxCacheKeysDefault, true}
 	c.keyCost = 100
 	return &c
 }
 
+// RunServer starts a server from the config
 func (c *Config) RunServer() (chan error, *http.Server) {
 	logger := c.getLogger()
 	log.Logger = logger
-	server := c.GetServer()
+	server := c.getServer()
 	var handler http.HandlerFunc = server.DecodeToken
 	loggingMiddleWare := hlog.NewHandler(logger)
-	serve := fmt.Sprintf(":%s", c.Port.get())
+	serve := fmt.Sprintf(":%s", c.port.get())
 	done := make(chan error)
 	srv := &http.Server{Addr: serve}
 	go func() {
@@ -74,8 +84,8 @@ func (c *Config) RunServer() (chan error, *http.Server) {
 	return done, srv
 }
 
-func (c *Config) GetServer() *decoder.Server {
-	jwksURL := c.JwksURL.get()
+func (c *Config) getServer() *decoder.Server {
+	jwksURL := c.jwksURL.get()
 	claimMappings := c.getClaimMappings()
 	jwsDec, err := decoder.NewJwsDecoder(jwksURL, claimMappings)
 	if err != nil {
@@ -87,20 +97,20 @@ func (c *Config) GetServer() *decoder.Server {
 	}
 	log.Info().Dict("mappings", claimMsg).Msg("mappings from claim keys to header")
 	cachedDec := decoder.NewCachedJwtDecoder(c.getCache(), jwsDec)
-	return decoder.NewServer(cachedDec, c.AuthHeader.get())
+	return decoder.NewServer(cachedDec, c.authHeader.get())
 }
 
 func (c *Config) getLogger() (logger zerolog.Logger) {
-	switch c.LogType.get() {
+	switch c.logType.get() {
 	case "json":
 		logger = zerolog.New(os.Stdout)
 	case "pretty":
 		logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout})
 	default:
-		panic(fmt.Errorf("unknown logger type %s", c.LogType.get()))
+		panic(fmt.Errorf("unknown logger type %s", c.logType.get()))
 	}
 	logger = logger.With().Timestamp().Caller().Logger()
-	level, err := zerolog.ParseLevel(c.LogLevel.get())
+	level, err := zerolog.ParseLevel(c.logLevel.get())
 	if err != nil {
 		panic(err)
 	}
@@ -108,7 +118,7 @@ func (c *Config) getLogger() (logger zerolog.Logger) {
 }
 
 func (c *Config) getCache() *ristretto.Cache {
-	keys := c.MaxCacheKeys.getInt64()
+	keys := c.maxCacheKeys.getInt64()
 	if keys < 1 {
 		panic(fmt.Errorf("Max keys need to be a positive number, was %d", keys))
 	}
@@ -126,12 +136,12 @@ func (c *Config) getCache() *ristretto.Cache {
 
 func (c *Config) getClaimMappings() map[string]string {
 	var claimMappings claimMappingsT = make(map[string]string)
-	path := c.ClaimMappingFilePath.get()
+	path := c.claimMappingFilePath.get()
 	errFile := claimMappings.fromFile(path)
 	if errFile != nil {
 		log.Warn().Err(errFile).Msgf("unable to load file resolving from env only")
 	}
-	errString := claimMappings.fromString(c.ClaimMappings.get())
+	errString := claimMappings.fromString(c.claimMappings.get())
 	if errString != nil {
 		log.Warn().Err(errString).Msgf("unable to parse claimMappingsEnv from env")
 		if errFile != nil {
